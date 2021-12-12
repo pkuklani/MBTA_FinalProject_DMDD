cf443154c4511603fb3afb24cd67b41114bad790
Saad Ghojaria <ghojariasaad@gmail.com>
2021-12-11 21:10:18 -0500

    Update sequences.sql

--- sequences.sql
+++ sequences.sql
@@ -1,3 +1,10 @@
 Create Sequence seq_mbta_traincurrentstatus start with 1 increment by 1;
 Create Sequence seq_mbta_station start with 1 increment by 1;
 Create Sequence seq_mbta_station start with 1 increment by 1;
+
+
+
+select * from mbta_passenger;
+
+
+select * from mbta_charlie
