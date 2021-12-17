069099169793407cd3f260699a0c53dccaf43a77
Saad Ghojaria <ghojariasaad@gmail.com>
2021-12-17 12:29:42 -0500

    insert-statement-saad

--- distance.sql
+++ distance.sql
@@ -1,58 +1,15 @@
-create or replace function  get_distance(Lat1 number(10,8), Long1 binary_float, Lat2 binary_float, Long2 binary_float)
-return binary_float 
-is
-d binary_float;
-lat_1 binary_float;
-long_1 binary_float;
-lat_2 binary_float;
-long_2 binary_float;
-begin
--- Convert to radians
-lat_1 := Lat1 / 57.2958;
-long_1 := Long1 / 57.2958;
-lat_2 :=  Lat2 / 57.2958;
-long_2 := Long2 / 57.2958;
--- Calc distance
-d := (Sin(lat_1) * Sin(long_1)) + (Cos(lat_2) * Cos(lat_2) * Cos(long_2 - long_1));
-if d <> 0 then
-d := 3958.75 * Atan(Sqrt(1 - power(d, 2)) / d);
-end if;
-return d;
-end;
-/
-select get_distance(13.077085f,80.262675f,13.065701f,80.258916f) as distance from dual;
+CREATE OR REPLACE FUNCTION distance (Lat1 IN NUMBER,
+                                     Lon1 IN NUMBER,
+                                     Lat2 IN NUMBER,
+                                     Lon2 IN NUMBER,
+                                     Radius IN NUMBER DEFAULT 3963) RETURN NUMBER IS
+ -- Convert degrees to radians
+ DegToRad NUMBER := 57.29577951;
 
+BEGIN
+  RETURN(NVL(Radius,0) * ACOS((sin(NVL(Lat1,0) / DegToRad) * SIN(NVL(Lat2,0) / DegToRad)) +
+        (COS(NVL(Lat1,0) / DegToRad) * COS(NVL(Lat2,0) / DegToRad) *
+         COS(NVL(Lon2,0) / DegToRad - NVL(Lon1,0)/ DegToRad))));
+END;
+commit;
 
-SELECT
-    10.2d,
-    32.7f
-FROM
-    dual;
-    
-    
-    select get_distance(13.077085f,80.262675f,13.065701f,80.258916f) as distance from dual;
-
-set serveroutput on;
-declare 
-lat_1 number(10,8);
-lat_2 number(10,8);
-long_1 number(10,8);
-long_2 number(10,8);
-Lat1 number(10,8):=13.077085;
-Long1 number(10,8):=80.262675;
-Lat2 number(10,8):=13.065701;
-Long2 number(10,8):=80.258916;
-d number(20,18);
-begin
--- Convert to radians
-lat_1 := Lat1 / 57.2958;
-long_1 := Long1 / 57.2958;
-lat_2 :=  Lat2 / 57.2958;
-long_2 := Long2 / 57.2958;
--- Calc distance
-d := (Sin(lat_1) * Sin(long_1)) + (Cos(lat_2) * Cos(lat_2) * Cos(long_2 - long_1));
-dbms_output.put_line(d);
-end;
-
-
-insert into mbta_login values (23222,3434,'check','sda');
--- insert-saad.sql
+++ insert-saad.sql
@@ -99,3 +99,57 @@ insert into mbta_schedule (ScheduleId, TrainId, RouteId, TrainTime) values (76, 
 insert into mbta_schedule (ScheduleId, TrainId, RouteId, TrainTime) values (59, 62, 6, '3:06:12 PM');
 
 commit
+
+
+
+call insert_into_mbta_station(7,'Heath Street',02130,42.3283433,-71.1127423,'Y',1,0);
+
+call insert_into_mbta_station(7,'Back of hill',02130,42.3294219,-71.1130323,'Y',2,1);
+
+call insert_into_mbta_station(7,'Riverway',02115,42.3315515,-71.1139492,'Y',3,2);
+
+call insert_into_mbta_station(7,'Mission Park',02115,42.3333418,-71.1112459,'Y',4,3);
+
+call insert_into_mbta_station(7,'Fenwood Road',02115,42.3352757,-71.1100743,'Y',5,4);
+
+call insert_into_mbta_station(7,'Brigham Circle',02120,42.3346986,-71.1055803,'Y',6,5);
+
+call insert_into_mbta_station(7,'Longwood Medical Area',02115,42.3333418,-71.1112459,'Y',7,6);
+
+call insert_into_mbta_station(7,'Museum Of Fine Arts',02114,42.3393849,-71.0962367,'Y',8,7);
+
+call insert_into_mbta_station(7,'Northeastern University',02115,42.3382446,-71.0907633,'Y',9,8);
+
+call insert_into_mbta_station(7,'Symphony',02115,42.3415241,-71.0890129,'Y',10,9);
+
+call insert_into_mbta_station(7,'Prudential',02115,42.3470606,-71.0839314,'Y',11,10);
+
+call insert_into_mbta_station(7,'Copley',02116,42.350251,-71.0797765,'Y',12,11);
+
+call insert_into_mbta_station(7,'Park Street',02108,42.3558238,-71.0650144,'Y',13,12);
+
+call insert_into_mbta_station(7,'Government Center',02115,42.3597271,-71.061591,'Y',14,13);
+
+call insert_into_mbta_station(7,'Haymarket',02108,42.3628689,-71.0604616,'Y',15,14);
+
+call insert_into_mbta_station(7,'North Station',02114,42.3664463,-71.0641627,'Y',16,15);
+
+call insert_into_mbta_station(7,'Science Park',02114,42.3668341,-71.0699427,'Y',17,16);
+
+call insert_into_mbta_station(7,'Lechmere',02115,42.370949,-71.07964,'Y',-1,17);
+
+
+
+
+Insert into ADMIN.MBTA_ROUTEINFO (ROUTEID,TYPEID,ROUTE_FROM,ROUTE_TO,DURATION) values (1,1,'Forest Hills','Oak Grove',37);
+Insert into ADMIN.MBTA_ROUTEINFO (ROUTEID,TYPEID,ROUTE_FROM,ROUTE_TO,DURATION) values (2,2,'Alewife','Ashmont',37);
+Insert into ADMIN.MBTA_ROUTEINFO (ROUTEID,TYPEID,ROUTE_FROM,ROUTE_TO,DURATION) values (3,2,'Alewife','Braintree',50);
+Insert into ADMIN.MBTA_ROUTEINFO (ROUTEID,TYPEID,ROUTE_FROM,ROUTE_TO,DURATION) values (4,3,'Boston College','Lechmere',45);
+Insert into ADMIN.MBTA_ROUTEINFO (ROUTEID,TYPEID,ROUTE_FROM,ROUTE_TO,DURATION) values (5,4,'Cleveland Circle','Lechmere',45);
+Insert into ADMIN.MBTA_ROUTEINFO (ROUTEID,TYPEID,ROUTE_FROM,ROUTE_TO,DURATION) values (6,5,'Riverside','Lechmere',45);
+Insert into ADMIN.MBTA_ROUTEINFO (ROUTEID,TYPEID,ROUTE_FROM,ROUTE_TO,DURATION) values (7,6,'Heath Street','Lechmere',45);
+
+
+
+
+
